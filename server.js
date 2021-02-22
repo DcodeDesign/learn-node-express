303c45ed8c84359fd7f2118a17f0fb4ddf89fae7
DcodeDesign <gravythomas@gmail.com>
2021-02-22 01:09:38 +0100

    tag 1.0.0

--- server.js
+++ server.js
@@ -1,64 +1,20 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const test = require("./test")
+const bodyParse = require("body-parser")
 
+const datas = require("./datas/datas")
 
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({ extended : true }))
 
-app.all("/", (req, res, next) => {
-    test()
-    next()
-})
+app.use(express.static("./assets"))
+app.engine('html', require('ejs').renderFile)
 
 app.get("/", (req, res, next) => {
-
-
-
-    res.write("bonjour je suis le get de /")
-    next()
+    res.render("index.ejs", {
+        infos: datas.cv
+    })
 })
-
-app.get("/", (req, res) => {
-    res.write("bonjour je suis le deuxieme get de /")
-    res.end()
-})
-
-app.post("/", (req, res) => {
-
-    res.end()
-})
-
-//http://localhost:3000/user/45/15/48
-app.get("/user/:id/:par1/:par2", (req, res) => {
-
-    res.status(200).json(req.params)
-})
-
-//http://localhost:3000/user?id=45&par1=15&par2=48
-app.get("/user", (req, res) => {
-    console.log(req.query)
-    res.status(200).json("/user")
-})
-
-
-app.get("/contact", (req, res) => {
-    res.end()
-})
-
-
-app.get("/forum", (req, res) => {
-    res.end()
-})
-
-
-app.post("/contact", (req, res) => {
-    res.end()
-})
-
-
-app.all("*", (req, res) => {
-    res.status(404).send("404")
-})
-
 
 app.listen(port, console.log(`Le serveur écoute sur le port ${port}`))
